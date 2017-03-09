3dcbec6bce313d1421f4d30d007c425f12a9f864
Colin Shield <pair+cshield@pivotal.io>
2017-03-09 17:33:13 +0000

    linuxified the recipe

--- recipes/git_duet.rb
+++ recipes/git_duet.rb
@@ -1,23 +1,27 @@
 #include_recipe 'homebrew'
-#include_recipe 'sprout-git::git_duet_global'
-#include_recipe 'sprout-git::git_duet_rotate_authors'
-#include_recipe 'sprout-git::authors'
+include_recipe 'sprout-git::git_duet_global'
+include_recipe 'sprout-git::git_duet_rotate_authors'
+include_recipe 'sprout-git::authors'
 
-#homebrew_tap 'git-duet/tap'
+working_file = '/tmp/linux_amd64.tar.gz'
+working_dir = '/tmp/git-duet'
 
-#package 'git-duet'
+remote_file working_file do
+  source 'https://github.com/git-duet/git-duet/releases/download/0.5.1/linux_amd64.tar.gz'
+  mode '0755'
+end
 
-#execute 'link git-duet' do
-#  command 'brew link git-duet'
-#  user    node['sprout']['user']
-#end
+bash 'extract_module' do
+  cwd ::File.dirname(working_file)
+  code <<-EOH
+    mkdir -p #{working_dir}
+    tar xzf #{working_file} -C #{working_dir}
+    mv #{working_dir}/* /usr/local/bin
+    EOH
+end
 
-#node['sprout']['git']['git_duet']['config'].each_pair do |setting, value|
-#  sprout_git_config setting do
-#    setting_value value
-#  end
-#end
-log 'message' do
-  message 'A message add to the log.'
-  level :info
+node['sprout']['git']['git_duet']['config'].each_pair do |setting, value|
+  sprout_git_config setting do
+    setting_value value
+  end
 end
